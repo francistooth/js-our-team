1a7fd54c92813f787d6f06945311fef1bb01f298
Francesco Dente <francesco.dente2003@gmail.com>
2024-06-18 16:12:50 +0200

    1.0.0: build: files upload, template html complete

---
+++ js/main.js
@@ -0,0 +1,47 @@
+const container = document.getElementById('card-container');
+
+// single person objects
+
+const team = [
+   {
+      'name': 'Waine Barnett',
+      'role': 'Founder & CEO',
+      'img': 'img/wayne-barnett-founder-ceo.jpg'
+   },
+
+   {
+      'name': 'Angela Caroll',
+      'role': 'Chief Editor',
+      'img': 'img/angela-caroll-chief-editor.jpg'
+   },
+
+   {
+      'name': 'Walter Gordon',
+      'role': 'Office Manager',
+      'img': 'img/walter-gordon-office-manager.jpg'
+   },
+
+   {
+      'name': 'Angela Lopez',
+      'role': 'Chief Editor',
+      'img': 'img/angela-lopez-social-media-manager.jpg'
+   },
+
+   {
+      'name': 'Scott Estrada',
+      'role': 'Developer',
+      'img': 'img/scott-estrada-developer.jpg'
+   },
+
+   {
+      'name': 'Barbara Ramos',
+      'role': 'Graphic Designer',
+      'img': 'img/barbara-ramos-graphic-designer.jpg'
+   }
+];
+
+for (let i = 0; i < team.length; i++) {
+   for (let j = 0; j < 3; j++) {
+      
+   }
+}
